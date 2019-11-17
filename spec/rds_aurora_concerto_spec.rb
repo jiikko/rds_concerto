91bb9516b685100ade2a5e0a03fd6f0e9f594d14
jiikko <n905i.1214@gmail.com>
2019-11-17 23:43:24 +0900

    Fix spec

--- spec/rds_aurora_concerto_spec.rb
+++ spec/rds_aurora_concerto_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RdsAuroraConcerto do
   it "has a version number" do
     expect(RdsAuroraConcerto::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
