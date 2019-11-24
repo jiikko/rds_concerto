5c9520d5edfa867963f670ef84ea4cf1b19a338f
jiikko <n905i.1214@gmail.com>
2019-11-24 19:52:36 +0900

    Impliment almost

--- lib/rds_aurora_concerto/aurora.rb
+++ lib/rds_aurora_concerto/aurora.rb
@@ -1,16 +1,112 @@
+require 'erb'
+
 class RdsAuroraConcerto::Aurora
-  def replica_list
+  def self.new
+    yaml = File.open('./.concert.yml')
+    hash = YAML.load(ERB.new(yaml.read).result)
+    client = Client.new(
+      config: Config.new(hash),
+      rds_client: Aws::RDS::Client.new(
+        region: 'ap-northeast-1',
+        access_key_id: hash['aws']['access_key_id'],
+        secret_access_key: hash['aws']['secret_access_key'],
+      )
+    )
   end
 
-  def cloned_list
+  class Config
+    attr_reader \
+      :source_identifier,
+      :source_cluster_identifier,
+      :region,
+      :aws_account_id
+    def initialize(hash)
+      @source_identifier = hash['source_instance']['identifier']
+      @source_cluster_identifier = hash['source_instance']['cluster_identifier']
+      @region = hash['region']
+      @aws_account_id = hash['aws']['account_id']
+    end
   end
 
-  def source_instance
-  end
+  class Client
+    attr_reader :rds_client, :config
 
-  def clone!
-  end
+    def initialize(rds_client: , config: )
+      @config = config
+      @rds_client = rds_client
+    end
 
-  def destroy!
+    def replica_list(condition: :all)
+      replica = self.all_list.select{|x| x[:name] == config.source_identifier }
+      case condition
+      when :all
+        replica
+      when :available
+        replica.select{|x| x[:status] == "available" }
+      end
+
+    def cloned_list
+      self.all_list.reject{|x| x[:name] == confgi.instance_identifier }
+    end
+
+    def all_list
+      rds_client.describe_db_instances.db_instances.map{ |x|
+        response = rds_client.list_tags_for_resource(resource_name: get_arn(identifier: x.db_instance_identifier))
+        {
+          name: x.db_instance_identifier,
+          size: x.db_instance_class,
+          engine: x.engine,
+          version: x.engine_version,
+          storage: x.allocated_storage,
+          endpoint: x.endpoint&.address,
+          status: x.db_instance_status,
+          created_at: x.instance_create_time,
+          tag: response.tag_list.map(&:to_hash),
+        }
+      }
+    end
+
+    def clone!(name: nil, instance: "", klass: "db.r4.large", identifier: nil)
+      unless name
+        name = "#{instance}-clone-#{Time.now.to_i}"
+      end
+      tags = [{
+        key: "created_by",
+        value: "#{identifier.nil? ? :cli : :web}_#{identifier || `hostname`.chomp}",
+      }]
+      rds_client.restore_db_cluster_to_point_in_time({
+        db_cluster_identifier: name,
+        source_db_cluster_identifier: config.source_cluster_identifier,
+        restore_type: "copy-on-write",
+        use_latest_restorable_time: true,
+        db_cluster_parameter_group_name: "concerto-aurora-56-cluster",
+        tags: tags
+      })
+      rds_client.create_db_instance({
+        db_instance_identifier: name,
+        db_cluster_identifier: name,
+        db_instance_class: klass,
+        engine: "aurora",
+        multi_az: false,
+        publicly_accessible: true,
+        db_subnet_group_name: "default",
+        db_parameter_group_name: "concerto-aurora-56",
+        tags: tags
+      });
+    end
+
+    def destroy!(name: nil, skip_final_snapshot: true)
+      return if [config.source_identifier,
+                 config.source_cluster_identifier].include?(name)
+
+      rds_client.delete_db_instance(db_instance_identifier: name, skip_final_snapshot: skip_final_snapshot)
+      rds_client.delete_db_cluster(db_cluster_identifier: name, skip_final_snapshot: skip_final_snapshot)
+    end
+
+    private
+
+    def get_arn(region: , identifier: )
+      "arn:aws:rds:#{config.region}:#{config.aws_account_id}:db:#{identifier}"
+    end
   end
 end
--- lib/rds_aurora_concerto/cli.rb
+++ lib/rds_aurora_concerto/cli.rb
@@ -1,2 +1,96 @@
+require 'thor'
+
 class RdsAuroraConcerto::CLI < Thor
+  desc "list", "レプリカやクローン一覧の閲覧"
+  def list
+    out = ''
+    out += show_replica
+    out << "\n"
+    out += show_clones
+    out << "\n"
+    puts out
+  end
+
+  desc "create NAME(レプリカを選択したい場合。指定しなければ適当に選びます)", "インスタンスの作成"
+  option :type, aliases: "-t", default: "db.r4.large", desc: "インスタンスタイプ"
+  def create(name = nil)
+    unless name
+      list = concerto.replica_list(condition: :available)[0][:name]
+      name = list[0][:name]
+    end
+    concerto.clone!(instance: name, klass: options[:type])
+  end
+
+  desc "destroy NAME(指定しなかったら全部消します)", "インスタンスの削除"
+  def destroy(name = nil)
+    if name
+      concerto.destroy!(name: name)
+    else
+      concerto.cloned_list.each{|i| concerto.destroy!(name: i[:name]) }
+    end
+  end
+
+  # desc "url NAME(URL を取得するインスタンスを指定したい場合。指定しなければ適当に選びます)", "インスタンスに接続するための URL の取得"
+  # def url(name = nil)
+  #   instance =
+  #     if name
+  #       concerto.clone_list.detect {|i| i[:name] == name }
+  #     else
+  #       concerto.clone_list.first
+  #     end
+  #   unless instance
+  #     puts "Instance is not existing."
+  #     exit(1)
+  #   end
+  #   url = `xxx`.chomp
+  #   if url == ""
+  #     puts "Please auth on heroku CLI."
+  #     exit(1)
+  #   end
+  #   uri = URI.parse(url)
+  #   uri.host = instance[:endpoint]
+  #   puts uri.to_s
+  # end
+
+  private
+
+  def show_replica
+    out = "-レプリカ-"
+    row = <<~EOH
+      -------
+      name: %{name}
+      size: %{size}
+      engine: %{engine}
+      version: %{version}
+      endpoint: %{endpoint}
+      status: %{status}
+      created_at: %{created_at}
+    EOH
+    concerto.replica_list.each do |hash|
+      out << row % hash
+    end
+    out
+  end
+
+  def show_clones
+    out = "-クローン-"
+    row = <<~EOH
+      -------
+      name: %{name}
+      size: %{size}
+      engine: %{engine}
+      version: %{version}
+      endpoint: %{endpoint}
+      status: %{status}
+      created_at: %{created_at}
+      tags: %{tag}
+    EOH
+    concerto.cloned_list.each do |hash|
+      out << row % hash
+    end
+    out
+  end
+
+  def concerto
+    @concerto ||= RdsAuroraConcerto::Aurora.new
 end
